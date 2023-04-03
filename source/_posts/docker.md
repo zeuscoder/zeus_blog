7b840ec7d3d8706efa01718c6a37c614ef40f3ab
zeuslv <zeuslv@wezhuiyi.com>
2023-04-03 23:42:46 +0800

    [feature] webpack demo

--- source/_posts/docker.md
+++ source/_posts/docker.md
@@ -1,5 +1,9 @@
 ---
-title: DevOps：Docker 容器笔记
+title: DevOps：Docker 笔记
 date: 2020-09-06 16:41:20
 tags:
 ---
+
+Docker 是一个用于开发，发布和运行应用程序的开放平台，也是跨平台的解决方案。
+
+<!-- more -->
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,5 +1,7 @@
 /** @type {import('webpack'.Configuration)} */
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ConsoleLogOnBuildWebpackPlugin = require('./plugin/ConsoleLogOnBuildWebpackPlugin');
 
 module.exports = {
     mode: 'development',
@@ -8,4 +10,8 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
+    plugins: [
+        new ConsoleLogOnBuildWebpackPlugin(),
+        new HtmlWebpackPlugin({ template: './src/index.html' }),
+    ],
 }
